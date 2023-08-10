620607d9337ca5f7404462034874a500ff018f76
Hirak Desai <53598504+hirak214@users.noreply.github.com>
2023-08-11 00:44:27 +0530

    Update: Task 3

--- Task2/my-app/src/App.tsx
+++ Task2/my-app/src/App.tsx
@@ -1,58 +1,65 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Layout, Menu, Input } from 'antd';
 import LoginPage from './LoginPage';
 import AboutMe from './AboutMe';
 import ContactMe from './ContactMe';
 
 const { Header, Content, Sider } = Layout;
+const { SubMenu } = Menu;
+
+// Sample menu data structure
+const menuData = [
+  {
+    key: '1',
+    title: 'Home',
+    children: [
+      {
+        key: '1-1',
+        title: 'Module 1',
+        children: [
+          {
+            key: '1-1-1',
+            title: 'Sub-Module 1',
+            children: [
+              {
+                key: '1-1-1-1',
+                title: 'Form Group 1',
+                children: [
+                  { key: '1-1-1-1-1', title: 'Form 1' },
+                  { key: '1-1-1-1-2', title: 'Form 2' },
+                  // ... more forms
+                ],
+              },
+              // ... more form groups
+            ],
+          },
+          // ... more sub-modules
+        ],
+      },
+      // ... more modules
+    ],
+  },
+  {
+    key: 'about',
+    title: 'About Me',
+  },
+  {
+    key: 'contact',
+    title: 'Contact Me',
+  },
+];
 
 class App extends Component {
-
-  // state = {
-  //   isLoggedIn: false,
-  // };
-
-  // handleLogout = () => {
-  //   this.setState({ isLoggedIn: false });
-  // };
-
-  // handleLogin = () => {
-  //   this.setState({ isLoggedIn: true });
-  // };
-
   render() {
-
-    // const { isLoggedIn } = this.state;
-
     return (
       <Router>
         <Routes>
-        {/* <Route
-            path="/"
-            element={
-              isLoggedIn ? (
-                <Navigate to="/dashboard" />
-              ) : (
-                <LoginPage onLogin={this.handleLogin} />
-              )
-            }
-          />
-          {isLoggedIn ? (
-            <>
-              <Route
-                path="/dashboard"
-                element={<Dashboard isLoggedIn={isLoggedIn} onLogout={this.handleLogout} />}
-              />
-              <Route path="/about" element={<AboutMe />} />
-              <Route path="/contact" element={<ContactMe />} />
-            </>
-          ) : null} */}
           <Route path="/" element={<LoginPage />} />
-              <Route path="/logout" element={<LoginPage />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/about" element={<AboutMe />} />
-              <Route path="/contact" element={<ContactMe />} />
+          <Route path="/logout" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/contact" element={<ContactMe />} />
         </Routes>
       </Router>
     );
@@ -60,10 +67,26 @@ class App extends Component {
 }
 
 class Dashboard extends Component {
+  // Filter logic
+  handleSearch = (value: string) => {
+    // Implement your search logic here
+  };
+
+  renderMenuItems = (menuItems: any[]) => { // Specify the type as 'any[]'
+    return menuItems.map((menuItem) => {
+      if (menuItem.children) {
+        return (
+          <SubMenu key={menuItem.key} title={menuItem.title}>
+            {this.renderMenuItems(menuItem.children)}
+          </SubMenu>
+        );
+      } else {
+        return <Menu.Item key={menuItem.key}>{menuItem.title}</Menu.Item>;
+      }
+    });
+  };
+
   render() {
-
-    // const { onLogout } = this.props;
-
     return (
       <Layout>
         <Header>
@@ -71,25 +94,16 @@ class Dashboard extends Component {
           <Menu theme="dark" mode="horizontal" style={{ display: 'flex', justifyContent: 'flex-end' }} defaultSelectedKeys={['1']}>
             <Menu.Item key="1">Username</Menu.Item>
             <Menu.Item key="/logout">
-            <Link to="/logout">
-            {/* <Link to="/logout" onClick={onLogout}> */}
-              Logout
-            </Link>
+              <Link to="/logout">Logout</Link>
             </Menu.Item>
           </Menu>
         </Header>
         <Layout>
           <Sider width={200} className="site-layout-background">
+            {/* Use Input.Search with the updated handleSearch */}
+            <Input.Search placeholder="Search Menu" onSearch={this.handleSearch} />
             <Menu mode="vertical" defaultSelectedKeys={['1']} style={{ height: '100%' }}>
-              <Menu.Item key="1">
-                <Link to="/dashboard">Home</Link>
-              </Menu.Item>
-              <Menu.Item key="about">
-                <Link to="/about">About Me</Link>
-              </Menu.Item>
-              <Menu.Item key="contact">
-                <Link to="/contact">Contact Me</Link>
-              </Menu.Item>
+              {this.renderMenuItems(menuData)}
             </Menu>
           </Sider>
           <Layout style={{ padding: '24px' }}>
@@ -109,5 +123,7 @@ class Dashboard extends Component {
     );
   }
 }
+
+
 
 export default App;
