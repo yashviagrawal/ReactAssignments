0d2ee2fa1d0feb8274d02a058f10576b50df123d
Yashvi Agrawal <74981992+yashviagrawal@users.noreply.github.com>
2023-08-17 23:02:57 +0530

    Update: refactoring

--- Task2/my-app/src/AboutMe.tsx
+++ Task2/my-app/src/AboutMe.tsx
@@ -19,18 +19,18 @@ const Sidebar: React.FC = () => {
     setSearchValue(event.target.value);
   };
 
-  const animals = document.getElementsByClassName('animals');
+  const menuItems = document.getElementsByClassName('menuItems');
 
-  const searchAnimal = () => {
-    const input = document.getElementById('searchbar') as HTMLInputElement;
-    const inputValue = input.value.toLowerCase();
+  const searchMenuItem = () => {
+    const searchBarInput = document.getElementById('searchbar') as HTMLInputElement;
+    const inputValue = searchBarInput.value.toLowerCase();
 
-    for (let i = 0; i < animals.length; i++) {
-      const animal = animals[i] as HTMLElement;
-      if (!animal.innerHTML.toLowerCase().includes(inputValue)) {
-        animal.style.display = 'none';
+    for (let i = 0; i < menuItems.length; i++) {
+      const menuItem = menuItems[i] as HTMLElement;
+      if (!menuItem.innerHTML.toLowerCase().includes(inputValue)) {
+        menuItem.style.display = 'none';
       } else {
-        animal.style.display = 'list-item';
+        menuItem.style.display = 'list-item';
       }
     }
   };
@@ -72,37 +72,37 @@ const Sidebar: React.FC = () => {
   }
   };
 
-  // services 
-  const handleCategoryClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
-    console.log("Reached in handleCategoryClick")
-    event.preventDefault();
-
-    const item = event.currentTarget.closest('.has-dropdown');
-    if (!item) {
-      return;
-    }
-
-    item.classList.toggle('opened');
-
-    Array.from(item.parentNode!.children).forEach((sibling) => {
-      if (sibling !== item) {
-        sibling.classList.remove('opened');
+    // services 
+    const handleCategoryClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+      console.log("Reached in handleCategoryClick")
+      event.preventDefault();
+  
+      const item = event.currentTarget.closest('.has-dropdown');
+      if (!item) {
+        return;
       }
-    });
-
-    const toOpen = item.querySelector('.sidebar-dropdown');
-    if (toOpen) {
-      toOpen.classList.toggle('active');
-    }
-
-    Array.from(item.parentNode!.children).forEach((sibling) => {
-      const toClose = sibling.querySelector('.sidebar-subdrop');
-      if (toClose) {
-        toClose.classList.remove('active');
+  
+      item.classList.toggle('opened');
+  
+      Array.from(item.parentNode!.children).forEach((sibling) => {
+        if (sibling !== item) {
+          sibling.classList.remove('opened');
+        }
+      });
+  
+      const toOpen = item.querySelector('.sidebar-dropdown');
+      if (toOpen) {
+        toOpen.classList.toggle('active');
       }
-    });
-  };
-
+  
+      Array.from(item.parentNode!.children).forEach((sibling) => {
+        const toClose = sibling.querySelector('.sidebar-subdrop');
+        if (toClose) {
+          toClose.classList.remove('active');
+        }
+      });
+    };
+  
 
   // subdrop down 
   const handleCategoryClickSub = (event: React.MouseEvent<HTMLAnchorElement>) => {
@@ -144,58 +144,59 @@ const Sidebar: React.FC = () => {
           name="search"
           type="text"
           className="form-control w-100 border-0"
+          // className="form-control w-100 border-0 bg-transparent"
           placeholder="Search here"
           value={searchValue}
           onChange={handleSearch}
-          onKeyUp={searchAnimal}
+          onKeyUp={searchMenuItem}
         />
       </div>
 
       <ul className="categories list-unstyled">
-        <li className="has-dropdown animals">
+        <li className="has-dropdown menuItems">
           <Link to="#" onClick={handleCategoryClickNav}>Dashboard</Link>
           <ul className="sidebar-dropdown list-unstyled">
-            <li className='animals'>
+            <li className='menuItems'>
               <Link to="#">Widget Dashboard</Link>
             </li>
-            <li className='animals'>
+            <li className='menuItems'>
               <Link to="#">Chart Dashboard</Link>
             </li>
-            <li className='animals'>
+            <li className='menuItems'>
               <Link to="#">Real-Time Dashboard</Link>
             </li>
           </ul>
         </li>
         
-    <li className="animals">
+    <li className="menuItems">
       <Link to="/about">About Us</Link>
     </li>
 
 
-    <li className="animals">
+    <li className="menuItems">
         <Link to="/contact">Contact Us</Link>
 
       </li>
 
 
-    <li className="has-dropdown animals subdrop">
+    <li className="has-dropdown menuItems subdrop">
       <Link onClick={handleCategoryClick} to="#"> Services</Link>
       <ul className="sidebar-dropdown list-unstyled">
-        <li className="has-dropdown animals has-subdrop">
+        <li className="has-dropdown menuItems has-subdrop">
           <Link onClick={handleCategoryClickSub} to="#">Web Development</Link>
-            <ul className="sidebar-dropdown list-unstyled animals sidebar-subdrop">
-                <li className='animals'><Link to="#">React JS</Link></li>
-                <li className='animals'><Link to="#">Angular JS</Link></li>
+            <ul className="sidebar-dropdown list-unstyled menuItems sidebar-subdrop">
+                <li className='menuItems'><Link to="#">React JS</Link></li>
+                <li className='menuItems'><Link to="#">Angular JS</Link></li>
               </ul>
         </li>
-        <li className="has-dropdown animals has-subdrop">
+        <li className="has-dropdown menuItems has-subdrop">
           <Link onClick={handleCategoryClickSub} to="#">Android Development</Link>
-            <ul className="sidebar-dropdown list-unstyled animals sidebar-subdrop">
-                <li className='animals'><Link to="#">React Native</Link></li>
-                <li className='animals'><Link to="#">Flutter</Link></li>
+            <ul className="sidebar-dropdown list-unstyled menuItems sidebar-subdrop">
+                <li className='menuItems'><Link to="#">React Native</Link></li>
+                <li className='menuItems'><Link to="#">Flutter</Link></li>
               </ul>
         </li>
-        <li className="animals"><Link to="#">DevOps</Link></li>
+        <li className="menuItems"><Link to="#">DevOps</Link></li>
 
 
       </ul>
